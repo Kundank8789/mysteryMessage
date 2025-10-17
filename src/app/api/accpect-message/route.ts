155e6cb53e84a8143cfc5630249cf7e4cb68320c
kundan <kundan8789@gmail.com>
2025-10-17 23:04:10 +0530

    accpect message test

--- src/app/api/accpect-message/route.ts
+++ src/app/api/accpect-message/route.ts
@@ -18,4 +18,30 @@ export async function POST(request: Request) {
 
     }
     const userId = user._id;
+    const {accpectMessage} = await request.json();
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            { accpectMessage: accpectMessage },
+            { new: true }
+        )
+        if (!updatedUser) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, { status: 404})
+        }
+        return Response.json({
+            success: true,
+            message: "accpectMessage updated successfully",
+            data: updatedUser
+        }, { status: 200})
+        
+    } catch (error) {
+        console.log("failed to update accpectMessage:")
+        return Response.json({
+            success: false,
+        message: "Failed to update accpectMessage"
+    }, { status: 500}) 
+    }
 }
