f46318d2d0269368d4c60cf774599dc3448949b5
kundan <kundan8789@gmail.com>
2025-10-25 21:24:53 +0530

    React hook form, shadcn and debouncing

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,22 @@
 'use client'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {useForm} from 'react-hook-form'
+import * as z from 'zod'
+import Link from 'next/link'
+import {useState} from 'react'
+import {useDebounceValue} from "usehooks-ts"
+import {useToast} from '@/components/ui/use-toast'
 
-import { useSession, signIn, signOut } from "next-auth/react"
-export default function components() {
-    const { data: session } = useSession()
-    if (session) {
-        return (
-            <>
-                Signed in as {session.user?.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        )
-    }
-    return (
-        <>
-            Not signed in <br />
-            <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-        </>
-    )
+const page = () => {
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const debouncedUsername = useDebounceValue(username, 500)
+  const {toast} = useToast()
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
